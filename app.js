6c20358c130a717586e169bfd11f143a4c4dfc92
Drew Wobeck <dwobeck@gmail.com>
2018-08-13 19:54:16 +0000

    put path added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var mongoURL = require('./mongoURL');
+var mongoURL = require('./mongoURL.js');
 
 const port = 8082;
 mongoose.connect(mongoURL);
@@ -19,7 +19,17 @@ db.once('open', function() {
             votes: { type: Number, default: 0 }
         }
     })
+    var userSchema = new mongoose.Schema({
+        email: String,
+        username: String,
+        date: Date,
+        accessLevel: String,
+        password: String,
+    });
+    
     var DoggoImg = mongoose.model('DoggoImg', doggoImgSchema);
+    var User = mongoose.model('User', userSchema);
+    
 const app = express();
 app.get('/doggo', (req,res) => {
     DoggoImg.find({}, function(err, doggoImg) {
@@ -39,6 +49,30 @@ app.post('/doggo', (req, res) => {
         console.log(doggo);
     });
 })
+app.put('/doggo', function(req, res) {
+    DoggoImg.findById(req.params.id, function(err, p) {
+        if(!p) {
+            return next(new Error('could not load document'));
+        }
+        else {
+            p.meta.votes += 1;
+            
+            p.save(function(err) {
+                if(err) {
+                    console.log('error');
+                }
+                else {
+                    console.log('success');
+                }
+            });
+        }
+    })
+});
+/*
+app.get('/user', (req, res) => {
+    User.findOne({'email': req.body.email}, function(err, user));
+})
+*/
 
 app.listen(port, function() {
     console.log('server is running on port' + port);
