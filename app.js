bb35dce8405ded54066deda1845f22aaee788a1c
Drew Wobeck <dwobeck@gmail.com>
2018-08-15 16:55:16 +0000

    connected to client with GET and POST methods, PUT not working yet

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var mongoURL = require('./mongoURL.js');
 
-const port = 8082;
+
+const port = (process.env.PORT || 8082);
 mongoose.connect(mongoURL);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
@@ -14,7 +14,8 @@ db.once('open', function() {
 })
     var doggoImgSchema = new mongoose.Schema({
         link: String,
-        date: { type: Date, default: Date.now },
+
+        date: { type: Date, default: new Date().toISOString() },
         meta: {
             votes: { type: Number, default: 0 }
         }
@@ -31,6 +32,17 @@ db.once('open', function() {
     var User = mongoose.model('User', userSchema);
     
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.options('/doggo', (req, res) => {
+    console.log("e");
+    res.send("e");
+})
 app.get('/doggo', (req,res) => {
     DoggoImg.find({}, function(err, doggoImg) {
         res.send(doggoImg);
@@ -38,35 +50,24 @@ app.get('/doggo', (req,res) => {
 })
 app.post('/doggo', (req, res) => {
     var link = req.body.link;
-    
+    console.log(req.body);
     var nuDoggo = new DoggoImg({
         link: link,
-        date: Date(now),
+
         
     });
     nuDoggo.save(function(err, doggo) {
         if(err) return console.error(err);
-        console.log(doggo);
+        res.send("saved! " + doggo);
     });
 })
 app.put('/doggo', function(req, res) {
-    DoggoImg.findById(req.params.id, function(err, p) {
-        if(!p) {
-            return next(new Error('could not load document'));
+    DoggoImg.findOneAndUpdate({_id: req.body.id}, {$set:{meta: {votes: meta.votes+1}}}, {new: true}, function(err, doc) {
+        if(err){
+            console.log("something went wrong with updating");
         }
-        else {
-            p.meta.votes += 1;
-            
-            p.save(function(err) {
-                if(err) {
-                    console.log('error');
-                }
-                else {
-                    console.log('success');
-                }
-            });
-        }
-    })
+        console.log(doc);
+    });
 });
 /*
 app.get('/user', (req, res) => {
